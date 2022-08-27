a3a9d2d54125f8c578bdade06486e94a99641c6b
Chris Davis <disscravis@chriss-mbp.lan>
2022-08-27 14:47:47 -0700

    almost donegit add .

--- src/app.jsx
+++ src/app.jsx
@@ -7,47 +7,56 @@ class App extends Component {
     super();
     this.state = {
       layout: Array(9).fill(null),
-      winnerState: false,
-      lastTurn: 'X'
+      gameOver: false,
+      lastTurn: 'X',
+      turnCounter: 1,
+      winnerMessage: null
       // 012(3), 345(12), 678(21), 036(9), 147(12), 258(15), 048(12), 246(12)
       // we'll have a function to check win/loss and update state for winnerstate
       // see if these combinations are about to be full and have same value
     };
     this.handleClick = this.handleClick.bind(this);
-    // console.log(this.state.layout[0] = true);
   }
 
   handleClick(i){
-    //slice to make copy of this.state.layout
-    //manipulate the index of the array --> i --> 3 newArr[i] = newValue
-    //setstate (newArr)
-    
-    // target-id
-    // selects the cell we click on
-    // to do turn based maybe use closure to remember what last invocation was
-    // if last was x, then assign value of td to O <= setSet()
-    // const newCohortNumbers = this.state.cohortNumbers.map(num=>num+1);
-    // this.setState({cohortNumbers: newCohortNumbers});
-    // console.log(e);
-    // console.log('clicked');
-    // variable => valid turn
-    // while !valid turn
-    let validTurn = false;
-    while (!validTurn){
     const newArr = this.state.layout.slice();
     if (newArr[i] === null) {
       newArr[i] = this.state.lastTurn;
       this.setState({layout: newArr});
-      validTurn = true;
+      if (this.state.lastTurn === 'O') this.setState({lastTurn: 'X'});
+      else this.setState({lastTurn: 'O'});
+      const newCounter = this.state.turnCounter + 1;
+      this.setState({turnCounter: newCounter});
+      console.log(this.state.turnCounter);
+      console.log(this.state.lastTurn);
     }
   }
-    if (this.state.lastTurn === 'O') this.setState({lastTurn: 'X'});
-    else this.setState({lastTurn: 'O'});
-    // const X = this.props.layout.map((placement, index, array) => {
-    //   // if (array[i]=== placement) placement = 'X';
-    //   console.log(placement);
-    // });
-    // this.setState({layout: X});
+
+  componentDidUpdate() {
+    const app = this.state;
+    if (!app.gameOver && app.turnCounter > 4) {
+      //
+      // 012, 345, 678, 036, 147, 258, 048, 246
+      // we'll have a function to check win/loss and update state for winnerstate
+      // see if these combinations are about to be full and have same value
+      if ((app.layout[0] === app.layout[1] && app.layout[1] === app.layout[2] && app.layout[2] !== null) ||
+          (app.layout[3] === app.layout[4] && app.layout[4] === app.layout[5] && app.layout[5] !== null) ||
+          (app.layout[6] === app.layout[7] && app.layout[7] === app.layout[8] && app.layout[8] !== null) ||
+          (app.layout[0] === app.layout[3] && app.layout[3] === app.layout[6] && app.layout[6] !== null) ||
+          (app.layout[1] === app.layout[4] && app.layout[4] === app.layout[7] && app.layout[7] !== null) ||
+          (app.layout[2] === app.layout[5] && app.layout[5] === app.layout[8] && app.layout[8] !== null) ||
+          (app.layout[0] === app.layout[4] && app.layout[4] === app.layout[8] && app.layout[8] !== null) ||
+          (app.layout[2] === app.layout[4] && app.layout[4] === app.layout[6] && app.layout[6] !== null)
+      ) {
+        const winner = this.state.lastTurn === 'X' ? 'O' : 'X';
+        const winnerMessage2 = `Player ${winner} won!!!`;
+        this.setState({winnerMessage: winnerMessage2});
+        // set state
+      }
+
+    }
+
+
   }
   
   render() {
@@ -57,6 +66,7 @@ class App extends Component {
           <h1>Tic Tac Toe</h1>
         </div>
         <Board layout = {this.state.layout} key = {'Cell 0'} handleClick={this.handleClick}/>
+        {/* <p>{this.state.winnerMessage}</p> */}
       </>
     );
   }
@@ -69,26 +79,12 @@ class Board extends Component {
     super();
   }
 
- 
-
-  // maybe give each cell unique id
-  // access the props object to manipulate text inside Cell class
-  // assign each cell a number and an array of two symbols to switch between
-  //<Cell value={this.state.layout[0]} onCLick= {this.HandleClick}/>
-  //<Cell value={this.state.layout[1]} onCLick= {this.HandleClick}/>
-
-  // <tr><Cell/><Cell/><Cell/></tr>
-  // <tr><Cell/><Cell/><Cell/></tr>
-  // <tr><Cell/><Cell/><Cell/></tr>
-
   render() {
-    // console.log(this.props,'object');
-    // console.log(this.props.layout,'array');
+   
     const cellStructure = [];
-    // make a row variable
-    // row.repeat
+  
     for (let i = 0; i < 9; i+=3){
-      // i = 0, i = 3, i = 6
+    
       const row = <tr>
         <Cell value={this.props.layout[i]} key={`Cell ${i + 1}`} handleClick={this.props.handleClick} i={i}/>
         <Cell value={this.props.layout[i + 1]} key={`Cell ${i + 2}`} handleClick={this.props.handleClick} i={i + 1}/>
@@ -97,14 +93,6 @@ class Board extends Component {
       cellStructure.push(row);
     }
 
-    // for (let i = 0; i < this.state.cohortNumbers.length; i++){
-    //   boxes.push(<Box 
-    //                number={this.state.cohortNumbers[i]} 
-    //                handleClick={this.handleClick} 
-    //                handleDelete={this.handleDelete} 
-    //                key={`Box${i}`} 
-    //              />);
-    // }
 
     // we want the Cell component to have access to the state of the app, which is two levels above
     // this state needs to be passed into value so we can alter the text displayed on the cell
@@ -127,26 +115,11 @@ class Cell extends Component {
     super();
   }
 
-  // HandleClick(e){
-  //   // target-id
-  //   // selects the cell we click on
-  //   // to do turn based maybe use closure to remember what last invocation was
-  //   // if last was x, then assign value of td to O <= setSet()
-  //   // const newCohortNumbers = this.state.cohortNumbers.map(num=>num+1);
-  //   // this.setState({cohortNumbers: newCohortNumbers});
-  //   // console.log(e);
-  //   // console.log('clicked');
-  //   // const X = this.props.layout.map((placement, index, array) => {
-  //   //   // if (array[i]=== placement) placement = 'X';
-  //   //   console.log(placement);
-  //   // });
-  //   // this.setState({layout: X});
-  //   // this.setState({layout: 'X'});
-  // }
+
   // access prop object here
   
   render() {
-    // console.log(this.props);
+    
     return (
       <td><button onClick={() => this.props.handleClick(this.props.i)}>{this.props.value}</button></td>
     );
@@ -171,15 +144,5 @@ class Cell extends Component {
 // state will also have a wincondition property
 // board will have access to state
 
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>Tic Tac Toe</h1>
-//       </div>
-//     );
-//   }
-// }
 
 render(<App />, document.querySelector('#root'));
